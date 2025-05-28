6632f33df5c3f66a21cbf04295107e77eda15a22
ARITRIK GHOSH <109242925+GHOSH2341@users.noreply.github.com>
2025-05-28 20:18:38 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from 'cors';
+import authRoutes from './routes/authRoutes.js';
+import patientRoutes from './routes/patientRoutes.js';
+import doctorRoutes from './routes/doctorRoutes.js';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/patients', patientRoutes);
+app.use('/api/doctors', doctorRoutes);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
